bcff1a7fe3d7c7682cb3ebebc6c8b85a59594aad
Jon Humphreys <jonhumphreys@chapman.edu>
2025-11-10 16:19:01 -0800

    Added chimp

---
+++ Assets/Scripts/TEMPChimpanzee.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Chimpanzee : Animal
+{
+    public override void MakeSound()
+    {
+        MonoBehaviour.print("The chimpanzee says: I bit your face off");
+    }
+}
